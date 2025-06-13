567b8828e2976fbb085f3d2465ff8c18862105a1
Anton Knysh <anton.knysh@andevart.com>
2025-06-13 14:05:41 +0300

    added prettier and TS to BE

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json('hello');
+});
+
+app.listen(3001, () => {
+  console.log('Running on 3001');
+});
