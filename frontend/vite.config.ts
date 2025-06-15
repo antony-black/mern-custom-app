6d90fdeaf1a27d7eac39438d434f240309ede75a
Anton Knysh <anton.knysh@andevart.com>
2025-06-15 08:23:41 +0300

    Added support for old browsers

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
-import react from '@vitejs/plugin-react';
 import * as path from 'node:path';
+
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['> 0.01%'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
