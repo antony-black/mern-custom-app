2959e9bb250ce03edfde5a4290cbdcecf9e523e0
Anton Knysh <anton.knysh@andevart.com>
2025-06-17 08:50:47 +0300

    Run the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,25 @@
-import express from 'express';
+// import path from 'path';
+
+import dotenv from "dotenv";
+import express from "express";
+
+import { connectDB } from "./config/db";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.json('hello');
+// const __dirname = path.resolve();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("hello");
 });
 
-app.listen(3001, () => {
-  // console.log('Running on 3001');
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Running on 3001");
 });
+// uSLCRsVaQQ49Hh1t
