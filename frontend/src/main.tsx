d7d7177d5b1860b568d00ea367445341d8d34274
Anton Knysh <anton.knysh@andevart.com>
2025-06-20 11:34:37 +0300

    Added the "ProductCard"/ "BrowserRouter"

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { ChakraProvider } from "@chakra-ui/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
