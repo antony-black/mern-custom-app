13137a360ae41914b64f1a506a980f31c06c2264
Anton Knysh <anton.knysh@andevart.com>
2025-10-02 14:43:12 +0300

    Removed redundant code from BE

--- backend/src/routes/products-router.ts
+++ backend/src/routes/products-router.ts
@@ -1,16 +1,9 @@
 import express from "express";
-import {
-  getAllProducts,
-  addProduct,
-  updateProduct,
-  removeProduct,
-  getFullProduct,
-} from "../controllers/products-controller";
+import { getAllProducts, addProduct, updateProduct, removeProduct } from "../controllers/products-controller";
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
-router.get("/:id", getFullProduct);
 router.post("/", addProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", removeProduct);
